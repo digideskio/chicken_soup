23bef86978fbd77d55a1b8551653d5272320f671
jfelchner <jeff@thekompanee.com>
2011-03-01 16:49:22 -0600

    Add Draft SVN Capability

---
+++ lib/chicken_soup/capabilities/svn.rb
@@ -0,0 +1,34 @@
+######################################################################
+#                          SUBVERSION TASKS                          #
+######################################################################
+Capistrano::Configuration.instance(:must_exist).load do
+  namespace :vc do
+    desc <<-DESC
+    DESC
+    task :log do
+      set :vc_log, `svn log -r #{previous_revision.to_i + 1}:#{current_revision}`
+    end
+  end
+end
+
+######################################################################
+#                         SUBVERSION DEFAULTS                        #
+######################################################################
+Capistrano::Configuration.instance(:must_exist).load do
+  namespace :capabilities do
+    namespace :defaults do
+      desc "[internal] Sets intelligent version control defaults for deployments"
+      task :svn do
+        set :scm,                         :subversion
+
+        # _cset :github_account,            ENV["USER"]
+        # _cset :deploy_via,                :remote_cache
+        # _cset(:repository)                {"git@github.com:#{github_account}/#{application}.git"}
+        # _cset(:branch)                    { `git branch`.match(/\* (\S+)\s/m)[1] || raise("Couldn't determine current branch") }
+        # _cset(:remote)                    { `git remote`.match(/(\S+)\s/m)[1] || raise("Couldn't determine default remote repository") }
+
+        # ssh_options[:forward_agent]       = true
+      end
+    end
+  end
+end

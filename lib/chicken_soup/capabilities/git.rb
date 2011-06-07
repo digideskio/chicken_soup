6e2aae3f89a26e6a4ed487c9d89dd6f4ce2940d1
Jeff Felchner <jeff@thekompanee.com>
2011-06-07 18:52:18 -0500

    Organize

--- lib/chicken_soup/capabilities/git.rb
+++ lib/chicken_soup/capabilities/git.rb
@@ -19,14 +19,13 @@ Capistrano::Configuration.instance(:must_exist).load do
     namespace :defaults do
       desc "[internal] Sets intelligent version control defaults for deployments"
       task :git do
-        set :scm,                         :git
-
         _cset :github_account,            ENV["USER"]
         _cset :deploy_via,                :remote_cache
-        _cset(:repository)                {"git@github.com:#{github_account}/#{application}.git"}
-        _cset(:branch)                    { `git branch`.match(/\* (\S+)\s/m)[1] || raise("Couldn't determine current branch") }
-        _cset(:remote)                    { `git remote`.match(/(\S+)\s/m)[1] || raise("Couldn't determine default remote repository") }
 
+        set :scm,                         :git
+        set(:repository)                  {"git@github.com:#{github_account}/#{application}.git"}
+        set(:branch)                      { `git branch`.match(/\* (\S+)\s/m)[1] || raise("Couldn't determine current branch") }
+        set(:remote)                      { `git remote`.match(/(\S+)\s/m)[1] || raise("Couldn't determine default remote repository") }
         ssh_options[:forward_agent]       = true
       end
     end

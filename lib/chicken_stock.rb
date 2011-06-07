f4a1d509ac12e8940c25840fa2054332a3bb6598
Jeff Felchner <jeff@thekompanee.com>
2011-06-07 18:52:48 -0500

    Consistency

--- lib/chicken_stock.rb
+++ lib/chicken_stock.rb
@@ -1,5 +1,6 @@
-require "chicken_soup/global"
-require "chicken_soup/environment"
+require 'chicken_soup/global'
+require 'chicken_soup/logger'
+require 'chicken_soup/environment'
 require 'chicken_soup/capabilities'
 require 'chicken_soup/notifiers'
-require "chicken_soup/deploy"
+require 'chicken_soup/deploy'

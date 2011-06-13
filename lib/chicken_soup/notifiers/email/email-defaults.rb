49002272ab64cba60210693df445341f1558567d
Jeff Felchner <jeff@thekompanee.com>
2011-06-13 01:19:23 -0500

    First pass at email notifier

--- lib/chicken_soup/notifiers/email/email-defaults.rb
+++ lib/chicken_soup/notifiers/email/email-defaults.rb
@@ -2,38 +2,51 @@
 #                       EMAIL NOTIFIER DEFAULTS                      #
 ######################################################################
 require 'mail'
+require 'erb'
+require 'chicken_soup/notifiers/email_notifier'
 
 Capistrano::Configuration.instance(:must_exist).load do |cap|
+  before    'notifiers:defaults:email',            'vc:log'
+
   namespace :notifiers do
     namespace :defaults do
       task :email do
-        _cset(:email_notifier_domain)         {ENV['CHICKEN_SOUP_EMAIL_DOMAIN']}
-        _cset(:email_notifier_username)       {ENV['CHICKEN_SOUP_EMAIL_USERNAME']}
-        _cset(:email_notifier_password)       {ENV['CHICKEN_SOUP_EMAIL_PASSWORD']}
+        _cset :email_notifier_format,              'html'
 
-        _cset :email_notifier_method,         :smtp
-        _cset :email_notifier_server,         "smtp.gmail.com"
-        _cset :email_notifier_port,           587
-        _cset :email_notifier_authentication, 'plain'
+        _cset :email_notifier_mail_method,         :smtp
+        _cset :email_notifier_server,              "smtp.gmail.com"
+        _cset :email_notifier_port,                587
+        _cset :email_notifier_authentication,      'plain'
 
-        _cset :email_notifier_options,      { :address              => email_notifier_server,
-                                              :port                 => email_notifier_port,
-                                              :domain               => email_notifier_domain,
-                                              :user_name            => email_notifier_username,
-                                              :password             => email_notifier_password,
-                                              :authentication       => email_notifier_authentication,
-                                              :enable_starttls_auto => true  }
+        _cset(:email_notifier_mail_options)      do
+                                                    { :address              => email_notifier_server,
+                                                      :port                 => email_notifier_port,
+                                                      :domain               => email_notifier_domain,
+                                                      :user_name            => email_notifier_username,
+                                                      :password             => email_notifier_password,
+                                                      :authentication       => email_notifier_authentication,
+                                                      :enable_starttls_auto => true  }
+                                                 end
 
-        _cset :email_notifier_sender,         'theshadow@theshadowknows.com'
-        _cset :email_notifier_subject,        "#{application.capitalize} has been deployed to #{rails_env.capitalize}"
-        _cset :email_notifier_body,           <<-WERNERHEISENBERG
-This is my deployment email
-                                              WERNERHEISENBERG
+        _cset :email_notifier_client_recipients,   []
+        _cset :email_notifier_internal_recipients, []
+        _cset :email_notifier_sender,              'theshadow@theshadowknows.com'
+        _cset :email_notifier_subject,             "#{application.titleize} has been deployed to #{rails_env.capitalize}"
+        _cset :email_notifier_client_template,     read_template("client_email.#{email_notifier_format}.erb")
+        _cset :email_notifier_internal_template,   read_template("internal_email.#{email_notifier_format}.erb")
 
-        Mail.defaults do
-          delivery_method cap[:email_notifier_method], cap[:email_notifier_options]
-        end
+        email_notifier = ChickenSoup::EmailNotifier.new(cap)
+        _cset :email_notifier_client_body,         render_erb(email_notifier_client_template, email_notifier)
+        _cset :email_notifier_internal_body,       render_erb(email_notifier_internal_template, email_notifier)
       end
     end
   end
+end
+
+def render_erb(template, email_info)
+  ERB.new(template, 0, "%<>").result(binding)
+end
+
+def read_template(template)
+  File.read(File.join(File.dirname(__FILE__), '..', '..', 'templates', template))
 end
--- lib/chicken_soup/notifiers/email/email-tasks.rb
+++ lib/chicken_soup/notifiers/email/email-tasks.rb
@@ -4,19 +4,33 @@
 require 'mail'
 
 Capistrano::Configuration.instance(:must_exist).load do |cap|
-  before    'notify:by_email',            'vc:log'
-  before    'deploy:clean',               'notify:by_email'
+  after     'deploy:cleanup',             'notify:by_email'
 
   namespace :notify do
     desc <<-DESC
       [internal] Sends a notification via email once a deployment is complete.
     DESC
     task :by_email do
-      Mail.deliver do
-             to cap[:email_notifier_recipients]
-           from cap[:email_notifier_sender]
-        subject cap[:email_notifier_subject]
-           body cap[:email_notifier_body]
+      Mail.defaults do
+        delivery_method cap[:email_notifier_mail_method], cap[:email_notifier_mail_options]
+      end
+
+      if !cap[:email_notifier_client_recipients].empty?
+        Mail.deliver do
+              to cap[:email_notifier_client_recipients]
+            from cap[:email_notifier_sender]
+         subject cap[:email_notifier_subject]
+            body cap[:email_notifier_client_body]
+        end
+      end
+
+      if !cap[:email_notifier_internal_recipients].empty?
+        Mail.deliver do
+              to cap[:email_notifier_internal_recipients]
+            from cap[:email_notifier_sender]
+         subject cap[:email_notifier_subject]
+            body cap[:email_notifier_internal_body]
+        end
       end
     end
   end

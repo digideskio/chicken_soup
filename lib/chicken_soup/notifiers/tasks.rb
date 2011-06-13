746de2cf9c17d6b84407472f45ab5c989d6d6a98
Jeff Felchner <jeff@thekompanee.com>
2011-06-13 03:31:03 -0500

    Load notifier tasks when deploy starts

--- lib/chicken_soup/notifiers/tasks.rb
+++ lib/chicken_soup/notifiers/tasks.rb
@@ -1,10 +1,10 @@
 Capistrano::Configuration.instance(:must_exist).load do
   before  'notifiers:load_tasks',      'load_notifier_tasks'
 
-  namespace :capabilities do
+  namespace :notifiers do
     desc <<-DESC
       [internal] A helper task used to load all of the tasks associated with the
-      requested capabilities.
+      requested notifiers.
     DESC
     task :load_tasks do
     end

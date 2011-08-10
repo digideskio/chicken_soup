f6741d6cc8149212480056dca8191f04f2c76eb9
Jeff Felchner <jeff@thekompanee.com>
2011-08-09 21:54:55 -0500

    Update task descriptions

--- lib/chicken_soup/capabilities/bundler/bundler-tasks.rb
+++ lib/chicken_soup/capabilities/bundler/bundler-tasks.rb
@@ -7,19 +7,24 @@ Capistrano::Configuration.instance(:must_exist).load do
   run_task  'bundler:install',    :as => manager_username
 
   namespace :gems do
-    desc "Install Bundled Gems"
+    desc "Processes the file containing all of the gems that you want installed and installs them one-by-one."
     task :install, :roles => :app do
       run_with_rvm rvm_ruby_string, "bundle install --gemfile #{latest_release}/Gemfile --path #{gem_packager_gem_path} --deployment --without development test"
     end
 
-    desc "Update Bundled Gems"
+    desc <<-DESC
+      Checks for the newest version of each gem and installs it.
+
+      > Note: This can never be used in production.  If you really wish to do this,
+      > you'll need to log into the server manually or use Capistrano's `console` task.
+    DESC
     task :update, :roles => :app do
       abort "I'm sorry Dave, but I can't let you do that. I have full control over production." if rails_env == 'production'
 
       run "cd #{latest_release} && bundle update"
     end
 
-    desc "Remove Bundled Gems"
+    desc "Removes all of the gems currently installed."
     task :clean, :roles => :app do
       run "rm -rf #{gem_packager_gem_path}/*"
     end

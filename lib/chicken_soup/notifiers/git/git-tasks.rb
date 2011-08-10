620ebb6a8e20f19f299a297877d1073624d1acf4
Jeff Felchner <jeff@thekompanee.com>
2011-08-10 03:02:49 -0500

    Standardize notification task names

--- lib/chicken_soup/notifiers/git/git-tasks.rb
+++ lib/chicken_soup/notifiers/git/git-tasks.rb
@@ -2,22 +2,24 @@
 #                         GIT NOTIFIER TASKS                         #
 ######################################################################
 Capistrano::Configuration.instance(:must_exist).load do
-  before  'deploy:cleanup',           'git:tag'
+  after     'deploy:cleanup',           'notify:via_git:tag'
 
-  namespace :git do
-    desc <<-DESC
-      Tags the deployed Git commit with the timestamp and environment it was deployed to.
+  namespace :notify do
+    namespace :via_git do
+      desc <<-DESC
+        Tags the deployed Git commit with the timestamp and environment it was deployed to.
 
-      The tag is auto-pushed to whatever `remote` is set to as well as `origin`.
-      Tag push happens in the background so it won't slow down deployment.
-    DESC
-    task :tag do
-      timestamp_string_without_seconds = Time.now.strftime("%Y%m%d%H%M")
-      tag_name = "deployed_to_#{rails_env}_#{timestamp_string_without_seconds}"
+        The tag is auto-pushed to whatever `remote` is set to as well as `origin`.
+        Tag push happens in the background so it won't slow down deployment.
+      DESC
+      task :tag do
+        timestamp_string_without_seconds = Time.now.strftime("%Y%m%d%H%M")
+        tag_name = "deployed_to_#{rails_env}_#{timestamp_string_without_seconds}"
 
-      `git tag -a -m "Tagging deploy to #{rails_env} at #{timestamp_string_without_seconds}" #{tag_name} #{branch}`
-      `git push #{remote} --tags > /dev/null 2>&1 &`
-      `git push origin --tags > /dev/null 2>&1 &`
+        `git tag -a -m "Tagging deploy to #{rails_env} at #{timestamp_string_without_seconds}" #{tag_name} #{branch}`
+        `git push #{remote} --tags > /dev/null 2>&1 &`
+        `git push origin --tags > /dev/null 2>&1 &`
+      end
     end
   end
 end

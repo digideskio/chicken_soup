3822940ba91f83330662a7f5f6111421e027469b
Jeff Felchner <jeff@thekompanee.com>
2011-08-11 01:41:06 -0500

    Rename the current Ruby environment string

--- lib/chicken_soup/capabilities/rvm/rvm-checks.rb
+++ lib/chicken_soup/capabilities/rvm/rvm-checks.rb
@@ -28,11 +28,11 @@ Capistrano::Configuration.instance(:must_exist).load do
         task :rvm do
           abort "Could not find an .rvmrc file at #{rvmrc_file}. To use the RVM capability, you must have a valid local .rvmrc file." unless File.exist?(rvmrc_file)
 
-          rvmrc_file_contents         = capture("cat #{current_path}/.rvmrc", :roles => :app)
-          set :current_rvm_ruby_string, rvmrc_file_contents.match(ChickenSoup::RVM_INFO_FORMAT)[1]
+          rvmrc_file_contents                 = capture("cat #{current_path}/.rvmrc", :roles => :app)
+          set :current_ruby_environment_string, rvmrc_file_contents.match(ChickenSoup::RVM_INFO_FORMAT)[1]
 
           unless ruby_version_update_pending
-            abort "'#{full_ruby_environment_string}' does not match the version currently installed on the server (#{current_rvm_ruby_string}).  Please run 'cap <environment> ruby:update deploy:subzero' if you would like to upgrade your Ruby version prior to deploying." unless current_rvm_ruby_string == rvm_ruby_string
+            abort "'#{full_ruby_environment_string}' does not match the version currently installed on the server (#{current_ruby_environment_string}).  Please run 'cap <environment> ruby:update deploy:subzero' if you would like to upgrade your Ruby version prior to deploying." unless current_ruby_environment_string == full_ruby_environment_string
           end
         end
       end

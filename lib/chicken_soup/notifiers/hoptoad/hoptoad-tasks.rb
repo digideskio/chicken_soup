9205b9f70030e1920160fe4220dedd2def94646c
Jeff Felchner <jeff@thekompanee.com>
2011-08-11 01:44:47 -0500

    Specify that notifications happen after deploy

--- lib/chicken_soup/notifiers/hoptoad/hoptoad-tasks.rb
+++ lib/chicken_soup/notifiers/hoptoad/hoptoad-tasks.rb
@@ -2,5 +2,6 @@
 #                       HOPTOAD NOTIFIER TASKS                       #
 ######################################################################
 Capistrano::Configuration.instance(:must_exist).load do
-  # require 'hoptoad_notifier/capistrano'
+  after "deploy",            "notify:via_airbrake"
+
 end

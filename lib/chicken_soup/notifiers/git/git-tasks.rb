59966690b9c3eb478b4cfb080c532ac9dcbea326
Jeff Felchner <jeff@thekompanee.com>
2011-08-11 02:10:41 -0500

    Use current_release timestamp when tagging

--- lib/chicken_soup/notifiers/git/git-tasks.rb
+++ lib/chicken_soup/notifiers/git/git-tasks.rb
@@ -19,10 +19,9 @@ Capistrano::Configuration.instance(:must_exist).load do
         Tag push happens in the background so it won't slow down deployment.
       DESC
       task :tag do
-        timestamp_string_without_seconds = Time.now.strftime("%Y%m%d%H%M")
-        tag_name = "deployment/#{rails_env}/#{timestamp_string_without_seconds}"
+        tag_name = "deployment/#{rails_env}/#{current_release}"
 
-        `git tag -a -m "Tagging deploy to #{rails_env} at #{timestamp_string_without_seconds}" #{tag_name} #{branch}`
+        `git tag -a -m "Tagging deploy to #{rails_env} at #{current_release}" #{tag_name} #{branch}`
         `git push #{remote} --tags > /dev/null 2>&1 &`
         `git push origin --tags > /dev/null 2>&1 &`
       end

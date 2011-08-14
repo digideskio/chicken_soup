1939084f72d6a724ddc3bf29e0f498cb31e9f7ac
Jeff Felchner <jeff@thekompanee.com>
2011-08-14 13:20:15 -0500

    Add proper task for RVM defaults

--- lib/chicken_soup/capabilities/rvm/rvm-defaults.rb
+++ lib/chicken_soup/capabilities/rvm/rvm-defaults.rb
@@ -8,20 +8,22 @@ end
 Capistrano::Configuration.instance(:must_exist).load do
   namespace :capabilities do
     namespace :defaults do
-      _cset :rvmrc_file,                    File.join(rails_root, '.rvmrc')
-      set   :ruby_version_update_pending,   false
+      task :rvm do
+        _cset :rvmrc_file,                    File.join(rails_root, '.rvmrc')
+        set   :ruby_version_update_pending,   false
 
-      _cset(:ruby_version)        do
-        contents = File.read(rvmrc_file)
-        contents.match(ChickenSoup::RVM_INFO_FORMAT)[2]
+        _cset(:ruby_version)        do
+          contents = File.read(rvmrc_file)
+          contents.match(ChickenSoup::RVM_INFO_FORMAT)[2]
+        end
+
+        _cset(:ruby_gemset)         do
+          contents = File.read(rvmrc_file)
+          contents.match(ChickenSoup::RVM_INFO_FORMAT)[3]
+        end
+
+        _cset(:full_ruby_environment_string)      {ruby_gemset ? "#{ruby_version}@#{ruby_gemset}" : ruby_version}
       end
-
-      _cset(:ruby_gemset)         do
-        contents = File.read(rvmrc_file)
-        contents.match(ChickenSoup::RVM_INFO_FORMAT)[3]
-      end
-
-      _cset(:full_ruby_environment_string)      {ruby_gemset ? "#{ruby_version}@#{ruby_gemset}" : ruby_version}
     end
   end
 end

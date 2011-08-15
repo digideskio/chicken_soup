680bb7ea5e73774c58e2c38099098c2d4756d661
Jeff Felchner <jeff@thekompanee.com>
2011-08-14 19:14:06 -0500

    Wrap in a Passenger default task

--- lib/chicken_soup/capabilities/passenger/passenger-defaults.rb
+++ lib/chicken_soup/capabilities/passenger/passenger-defaults.rb
@@ -10,7 +10,9 @@ end
 Capistrano::Configuration.instance(:must_exist).load do
   namespace :capabilities do
     namespace :defaults do
-      set :application_server_type,   :passenger
+      task :passenger do
+        set :application_server_type,   :passenger
+      end
     end
   end
 end
